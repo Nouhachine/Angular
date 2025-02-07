c292686d1b255006d779f8cae3c37efe8d16fde9
unknown <nouha>
2025-02-07 11:39:51 +0100

    workshop1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResidencesComponent } from './residences/residences.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResidencesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
