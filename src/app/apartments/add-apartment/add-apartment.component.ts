86a01654d342c22d5f1516888bd4415ba44156a1
unknown <nouha>
2025-02-21 12:11:55 +0100

    Workshop3+Validators

--- src/app/apartments/add-apartment/add-apartment.component.ts
+++ src/app/apartments/add-apartment/add-apartment.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-apartment',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-apartment.component.css']
 })
 export class AddApartmentComponent {
+  AddAppart= new FormGroup({
+    apartNum: new FormControl('',Validators.required),
+    floorNum: new FormControl('',[Validators.required,Validators.min(2)]),
+    surface: new FormControl(''),
+    terrace: new FormControl('yes'),
+    surfaceterrace: new FormControl(''),
+    category: new FormControl(''),
+    ResidenceId: new FormControl(''),
+  });
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { ResidencesComponent } from './residences/residences.component';
 import { ResidenceDetailsComponent } from './residences/residence-details/residence-details.component';
 import { AddResidenceComponent } from './residences/add-residence/add-residence.component';
 import { ApartmentsComponent } from './apartments/apartments.component';
 import { ApartmentsByResidenceComponent } from './apartments/apartments-by-residence/apartments-by-residence.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AddApartmentComponent } from './apartments/add-apartment/add-apartment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
     HomeComponent,
+    FooterComponent,
+    NotFoundComponent,
     ResidencesComponent,
     ResidenceDetailsComponent,
     AddResidenceComponent,
     ApartmentsComponent,
     ApartmentsByResidenceComponent,
-    NotFoundComponent
+    AddApartmentComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
